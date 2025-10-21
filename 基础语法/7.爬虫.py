baa1d5a727e4f66d70fef0643d04c864d78c3b25
夜林蓝 <1562098029@qq.com>
2025-10-22 00:09:26 +0800

    feat(crawler): 增强爬虫功能支持请求头和参数处理
    - 引入 fake_useragent 库用于生成随机 User-Agent
    - 新增测试函数 test_header 和 test_fake_agent 验证请求头设置
    - 实现 URL 编码与解码功能，新增 test_url_code 函数
    - 添加带参数的 GET 请求示例，支持动态搜索内容
    - 实现贴吧页面抓取并保存为 HTML 文件- 新增 POST 请求示例，用于上报统计数据- 更新主函数入口以执行新的测试逻辑

--- 基础语法/7.爬虫.py
+++ 基础语法/7.爬虫.py
@@ -1,4 +1,7 @@
+from urllib.parse import quote, unquote
+
 import requests
+from fake_useragent import UserAgent
 
 
 # 测试requests
@@ -6,12 +9,59 @@ def test_requests():
     url = "https://www.baidu.com"
     response = requests.get(url)
     print(response)
+    # response.encoding = "utf-8"
     # print(response.text )
     print(response.content.decode())
 
 
-if __name__ == '__main__':
+def test_jpg():
     url = "https://i2.hdslb.com/bfs/archive/4730097e10a79e46ea66593fd642e5d64ae37e34.jpg"
     response = requests.get(url)
     with open("resources/ayaka.jpg", "wb") as f:
         f.write(response.content)
+
+
+def test_header():
+    url = "https://www.baidu.com"
+    response = requests.get(url, headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"})
+    print(len(response.content.decode()))
+
+
+def test_fake_agent():
+    url = "https://www.baidu.com"
+    response = requests.get(url, headers={"User-Agent": UserAgent().random})
+    print(len(response.content.decode()))
+
+
+def test_url_code():
+    print(unquote("https://www.baidu.com/s?ie=UTF-8&wd=%E8%B1%86%E5%8C%85"))
+    print(quote("豆包"))
+
+
+def test_params():
+    url = "https://www.baidu.com"
+    s = input("请输入搜索内容：")
+    response = requests.get(url, headers={"User-Agent": UserAgent().random}, params={"wd": s})
+    print(response.content.decode())
+
+
+def test_tieba():
+    s = "犬夜叉"
+
+    url = f"https://so.gamersky.com/?s={s}"
+
+    response = requests.get(url, headers={"User-Agent": UserAgent().random})
+    with open("resources/tieba.html", "w", encoding="utf-8") as f:
+        f.write(response.content.decode())
+
+import json
+def test_only_grammar():
+    post1 = requests.post("https://item-statistics.gamersky.com/ad/statistics/event/report/online/5.0.0",
+                          headers={"User-Agent": UserAgent().random}, data={},
+                          proxies={"http": "http://127.0.0.1:8888"})
+    json_data = json.loads(post1.content.decode())
+
+
+if __name__ == '__main__':
+    test_only_grammar()
