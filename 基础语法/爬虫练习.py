1af2a473c11e457b1448025aeda6fecbe43cf376
夜林蓝 <1562098029@qq.com>
2025-10-31 00:40:27 +0800

    feat(crawler): 添加多个爬虫练习函数并引入selenium支持
    - 新增test24至test30共7个爬虫练习函数- 引入selenium库支持动态网页爬取- 添加chromedriver许可证文件
    - 实现表单数据、随机类名、加密数据等多种爬取场景
    - 增加数据处理和统计分析功能
    - 添加模拟用户行为的自动化操作
    - 支持验证码点击和页面交互操作

--- 基础语法/爬虫练习.py
+++ 基础语法/爬虫练习.py
@@ -1,12 +1,19 @@
 # coding=utf-8
 import base64
+import hashlib
+import json
+import random
 import re
 import time
 from re import findall
 
+import numpy as np
 import requests
 from fake_useragent import UserAgent
 from lxml import etree
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
 def test01():
@@ -330,4 +337,170 @@ def test23():
         print(" | ".join(ls))
 
 
-test23()
+def test24():
+    page_url = "https://spiderbuf.cn/web-scraping-practice/scraping-form-rpa"
+    html = requests.get(page_url,
+                        headers={"User-Agent": UserAgent().random, 'Referer': 'https://spiderbuf.cn/list'}).text
+    root = etree.HTML(html)
+    print(root.xpath('//form/div/label[@for="username"]/text()')[0],
+          root.xpath('//form/div/input[@name="username"]')[0].attrib["value"])
+    print(root.xpath('//form/div/label[@for="password"]/text()')[0],
+          root.xpath('//form/div/input[@name="password"]')[0].attrib["value"])
+    print(root.xpath('//form/div/label[@for="email"]/text()')[0],
+          root.xpath('//form/div/input[@name="email"]')[0].attrib["value"])
+    print(root.xpath('//form/div/label[@for="website"]/text()')[0],
+          root.xpath('//form/div/input[@name="website"]')[0].attrib["value"])
+    print(root.xpath('//form/div/label[@for="date"]/text()')[0],
+          root.xpath('//form/div/input[@name="date"]')[0].attrib["value"])
+    print(root.xpath('//form/div/label[@for="time"]/text()')[0],
+          root.xpath('//form/div/input[@name="time"]')[0].attrib["value"])
+    number_ = root.xpath('//form/div/input[@name="number"]')[0]
+    print(root.xpath('//form/div/label[@for="number"]/text()')[0],
+          number_.attrib["value"] if "value" in number_.attrib else "")
+    range_ = root.xpath('//form/div/input[@name="range"]')[0]
+    print(root.xpath('//form/div/label[@for="range"]/text()')[0],
+          range_.attrib["value"] if "value" in range_.attrib else "")
+    color_ = root.xpath('//form/div/input[@name="color"]')[0]
+    print(root.xpath('//form/div/label[@for="color"]/text()')[0],
+          color_.attrib["value"] if "value" in color_.attrib else "")
+    search_ = root.xpath('//form/div/input[@name="search"]')[0]
+    print(root.xpath('//form/div/label[@for="search"]/text()')[0],
+          search_.attrib["value"] if "value" in search_.attrib else "")
+    print(root.xpath('//form/div/label[@for="textarea"]/text()')[0],
+          root.xpath('//form/div/textarea[@name="textarea"]')[0].text)
+    print(root.xpath('//form/div/label[@for="gender"]/text()')[0],
+          root.xpath('//form/div/input[@name="gender" and @checked="true"]')[0].attrib["value"])
+    interest = root.xpath('//form/div/input[@name="interest" and @checked]')
+    print(root.xpath('//form/div/label[@for="interest"]/text()')[0],
+          ",".join([item.attrib["value"] for item in interest]))
+    print(root.xpath('//form/div/label[@for="country"]/text()')[0],
+          root.xpath('//form/div/select/option[@selected]')[0].text)
+    print(root.xpath('//form/div/p/text()')[0], " | ".join(root.xpath('//form/div/ul/li/a/text()')))
+
+
+def test25():
+    page_url = "https://spiderbuf.cn/web-scraping-practice/random-css-classname#google_vignette"
+    html = requests.get(page_url, headers={"User-Agent": UserAgent().random}).text
+    root = etree.HTML(html)
+    divs = root.xpath('//div[@class="FlDdlN" or @class="makOPE"]')
+    for div in divs:
+        print(div.xpath('string(.)').strip())
+
+
+def test26():
+    page_url = "https://spiderbuf.cn/web-scraping-practice/scraper-practice-c01"
+    html = requests.get(page_url, headers={"User-Agent": UserAgent().random
+        , 'Referer': 'https://spiderbuf.cn/web-scraping-practice/scraper-practice-c01'}).text
+    root = etree.HTML(html)
+    divs = root.xpath('//li/a[contains(text(),"mnist")]')
+    href_ = "https://spiderbuf.cn" + divs[0].attrib["href"]
+    html1 = requests.get(href_, headers={"User-Agent": UserAgent().random
+        , 'Referer': 'https://spiderbuf.cn/web-scraping-practice/scraper-practice-c01'
+        ,
+                                         'Cookie': '_ga=GA1.1.1615478358.1761312911; __cgf3t=G0gzgFKDRlLtmZH7NrzqOb1x4pek1xNQk12KKc4g21Y-1731624199; __gads=ID=f82407b378a1ce86:T=1761312909:RT=1761827398:S=ALNI_Maz3g1CvkLYyj5ZGiOwDwHpTyunXQ; __gpi=UID=000011a858720406:T=1761312909:RT=1761827398:S=ALNI_MYPrE9_2n-b1sLIQvWujnhtcJb7xQ; __eoi=ID=a01ab92a79a46f70:T=1761312909:RT=1761827398:S=AA-Afja7VCRfR75kLDj7Eg_SqCA8; FCCDCF=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%5B32%2C%22%5B%5C%22d48a62e3-c494-4579-9f0f-e8f2022f2271%5C%22%2C%5B1761735673%2C853000000%5D%5D%22%5D%5D%5D; FCNEC=%5B%5B%22AKsRol99oM-NxTAg0NwEAnpV5nAB1hr5C9pSwI7giGi891Ofh-cl45Xbs1xP_962V-0-nWM77veT8SEPrNS_4h-jnReD3agfnmC49S7XBcMAQwYDHopHUlbW8iu6j402F61A-fYPHTanzkgFRwQXHDNIDMdLrHgaNQ%3D%3D%22%5D%5D; _ga_7B42BKG1QE=GS2.1.s1761822496$o6$g1$t1761827545$j47$l0$h0'}).text
+    analysis_html(html1, "//table[@class='table']/thead/tr/th", "//table[@class='table']/tbody/tr")
+
+
+def test27():
+    page_url = "https://spiderbuf.cn/web-scraping-practice/scraper-practice-c02"
+    html = requests.get(page_url, headers={"User-Agent": UserAgent().random
+        , 'Referer': 'https://spiderbuf.cn/web-scraping-practice/scraper-practice-c01'}).text
+    findall = re.findall("const encryptedData = \"(.+)\";", html)[0]
+    list = json.loads(base64.b64decode(findall).decode())
+    print("出发地", "目的地", "票价")
+    for item in list["flights"]:
+        print(item["from"], item["to"], item["price"])
+
+
+def test28():
+    width_list = []
+    for page_num in range(1, 6):
+        print(f"正在爬取第{page_num}页")
+        random_value = random.randint(2000, 10000)
+        timestamp = int(time.time())
+        xorResult = page_num ^ timestamp
+        hash_value = hashlib.md5(f"{xorResult}{timestamp}".encode('utf-8')).hexdigest()
+
+        data = {
+            "xorResult": xorResult,
+            'random': random_value,
+            "timestamp": timestamp,
+            "hash": hash_value
+        }
+        page_url = "https://spiderbuf.cn/web-scraping-practice/scraper-practice-c03"
+        html = requests.post(page_url, headers={"User-Agent": UserAgent().random
+            , 'Referer': 'https://spiderbuf.cn/web-scraping-practice/scraper-practice-c03'
+            ,
+                                                'Cookie': '_ga=GA1.1.1615478358.1761312911; __cgf3t=G0gzgFKDRlLtmZH7NrzqOb1x4pek1xNQk12KKc4g21Y-1731624199; __gads=ID=f82407b378a1ce86:T=1761312909:RT=1761829624:S=ALNI_Maz3g1CvkLYyj5ZGiOwDwHpTyunXQ; __gpi=UID=000011a858720406:T=1761312909:RT=1761829624:S=ALNI_MYPrE9_2n-b1sLIQvWujnhtcJb7xQ; __eoi=ID=a01ab92a79a46f70:T=1761312909:RT=1761829625:S=AA-Afja7VCRfR75kLDj7Eg_SqCA8; FCCDCF=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%5B%5B32%2C%22%5B%5C%22d48a62e3-c494-4579-9f0f-e8f2022f2271%5C%22%2C%5B1761735673%2C853000000%5D%5D%22%5D%5D%5D; FCNEC=%5B%5B%22AKsRol9niu3ffqhe1knaof9r6Y1qk26PGDZ0QJ2P7Xcg5sslst61xMAxJtZUR0s85ngiqM9qRtUuXJxlPlcc0Tk-Z4evrmOisiz7NAovXKJaoZtWvKcnVqpdg5gfqY_ESQX6HrnNlKExXF1b_4pcMUR89UzZIX0RIA%3D%3D%22%5D%5D; _ga_7B42BKG1QE=GS2.1.s1761822496$o6$g1$t1761829748$j35$l0$h0'
+                                                }
+                             , json=data).text
+        print(html)
+        json_data = json.loads(html)
+        for item in json_data:
+            width_list.append(item["sepal_width"])
+    print(width_list)
+    print(f"平均值：{sum(width_list) / len(width_list)}")
+    print(f"最大值：{max(width_list)}")
+    print(f"最小值：{min(width_list)}")
+    print(f"总和: {sum(width_list)}")
+
+
+def test29():
+    page_url = "https://spiderbuf.cn/static/js/c04/lhY3nm7.min.js"
+    html = requests.get(page_url, headers={"User-Agent": UserAgent().random
+        , 'Referer': 'https://spiderbuf.cn/web-scraping-practice/scraper-practice-c01'}).text
+    print(html)
+    findall1 = re.findall("'from','(.+)','webdriver'", html)[0]
+    list2 = json.loads(base64.b64decode(findall1).decode()[:-2])
+    # 标题
+    print("标题", "内容", "阅读数", "点赞数", "分享数", "评论数")
+    view_comment = []
+    for item in list2:
+        print(item["title"], item["content"], item["reads"], item["likes"], item["shares"], item["comments"])
+        view_comment.append(int(item["reads"]) + int(item["comments"]))
+
+    print("平均阅读数：", sum(view_comment) / len(view_comment))
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+service = Service("./chrome/chromedriver.exe")
+
+def test30():
+    base_url = 'https://spiderbuf.cn/web-scraping-practice/scraper-practice-c04'
+    if __name__ == '__main__':
+        options = webdriver.ChromeOptions()
+        options.add_argument('disable-infobars')
+        options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
+
+        options.add_argument('--disable-blink-features=AutomationControlled')  # 改变navigator.webdriver 属性值
+
+        client = webdriver.Chrome(options=options, service=service)
+        print('Getting page...')
+        client.get(base_url)
+        time.sleep(3)
+
+        # 模拟用户在页面上滑动光标
+        actionChains = ActionChains(client)
+        actionChains.move_by_offset(430, 330)
+        for i in range(20):
+            step = random.randint(1, 10)
+            actionChains.move_by_offset(step, step).perform()
+
+        checkbox = client.find_element(By.ID, 'captcha')
+        checkbox.click()
+        print('Checkbox clicked...')
+        time.sleep(3)
+        html = client.page_source
+        # print(html)
+        client.quit()
+        root = etree.HTML(html)
+        items = root.xpath('//div[@class="stats"]')
+        results = []
+        for item in items:
+            spans = item.xpath('.//span')
+            s = ''.join(spans[3].xpath('string(.)'))
+            results.append(int(re.findall('\d+', spans[0].text)[0]) + int(''.join(re.findall('\d+', s))))
+        print(np.average(results))
+
+
+test30()
